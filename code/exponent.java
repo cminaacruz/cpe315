2e99d25484302e8a8638aa51fb2441a0baa21c69
Carmina Cruz <cminaacruz@gmail.com>
2019-04-12 16:44:31 -0700

    fixed exponent

--- code/exponent.java
+++ code/exponent.java
@@ -8,6 +8,8 @@ public class exponent {
     public static void exponent(){
         int base, exponent;
         int answer;
+        int num = 0;
+        int tmp;
         Scanner input = new Scanner(System.in);
         System.out.println("This program calculates exponents\n");
         System.out.print("Enter a base: ");
@@ -15,8 +17,16 @@ public class exponent {
         System.out.print("Enter an exponent: ");
         exponent = input.nextInt();
         answer = base;
-        for (int i = 1; i < exponent; i++)
-            answer += answer;
+
+        for (int i = 1; i < exponent; i++){
+            num = answer;
+            tmp = 0;
+            for (int j = 0; j < base; j++) {
+                tmp += num;
+            }
+            answer = tmp;
+        }
+
         System.out.println("Result: " + answer);
     }
 }
