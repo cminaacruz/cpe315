2eb1e2583206ff59b3f0afaa8e197fd89389e7bc
Gonzalo Arana <gonzalo.ar18@hotmail.com>
2019-04-26 00:42:51 -0700

    Finished code for writing Instructions as bits. Need to fix a few bugs.

---
+++ lab2/Instruction.java
@@ -0,0 +1,75 @@
+public class Instruction {
+	/*Constants for the op-codes for each possible command
+	  without register/immediate values filled*/
+	public static final int AND_CMD  = 0x00000024;
+	public static final int OR_CMD   = 0x00000025;
+	public static final int ADD_CMD  = 0x00000020;
+	public static final int SLT_CMD  = 0x0000002A;
+	public static final int SUB_CMD  = 0x00000022;
+
+	public static final int SLL_CMD  = 0x00000000;
+
+	public static final int ADDI_CMD = 0x20000000;
+	public static final int BEQ_CMD  = 0x10000000;
+	public static final int BNE_CMD  = 0x14000000;
+	public static final int LW_CMD   = 0x8C000000;
+	public static final int SW_CMD   = 0xAC000000;
+	public static final int J_CMD    = 0x08000000;
+	public static final int JAL_CMD  = 0x0C000000;
+
+	public static final int JR_CMD   = 0x00000008;
+
+	private int opCode = 0;
+
+	public Instruction(int opTemplate){
+		opCode = opTemplate; //set base op code
+	}
+
+	public void setRS(int regS){
+		//shift by 21 bits and or
+		opCode |= regS<<21;
+	}
+
+	public void setRT(int regT){
+		//shift by 16 bits and or
+		opCode |= regT<<16;
+	}
+
+	public void setRD(int regD){
+		//shift by 11 bits and or
+		opCode |= regD<<11;
+	}
+
+	public void setH(int h){
+		//shift by 6 bits and or
+		opCode |= h<<6;
+	}
+
+	public void setImd(int imd){
+		//just or
+		opCode |= imd;
+	}
+
+	public int getOpCode(){
+		return opCode;
+	}
+
+	public void printBinary(){
+		int tempCode = opCode;
+		int counter = 32;
+		while(counter != 0){
+			int bitVal = tempCode&0x80000000;
+			if(bitVal == 0)
+				System.out.print("0");
+			else
+				System.out.print("1");
+			tempCode = tempCode<<1;
+
+			if(counter%4 == 0)
+				System.out.print(" ");
+			counter--;
+		}
+		System.out.print("\n");
+	}
+
+}
--- lab2/asmParser.java
+++ lab2/asmParser.java
@@ -6,11 +6,123 @@ public class asmParser {
     // NOTE: created void functions but can change later to return hashmaps
 
     public static void main(String args[]) throws Exception {
-        getInstructions(args[0]);
-        // getRegisters();
-        //System.out.println(getLabels(args[0]));
+        Map<String, Integer> labels;
+        Map<String, Integer> registers;
+        List<ArrayList<String>> instrList;
+        int instrCount = 1;
 
+        labels = getLabels(args[0]);
+        instrList = getInstructions(args[0]);
+        registers = getRegisters();
 
+        //use instruction list to generate binary code
+        for(ArrayList instr: instrList){
+            if(instr.size() != 0){
+                int target = 0;
+                String command = instr.get(0).toString(); //get instruction command
+                //System.out.println("Instr " + instrCount + instr);
+                //System.out.println(instr);
+
+                switch(command){
+                    case "and":
+                        Instruction and = new Instruction(Instruction.AND_CMD);
+                        and.setRD(registers.get(instr.get(1).toString()));
+                        and.setRS(registers.get(instr.get(2).toString()));
+                        and.setRT(registers.get(instr.get(3).toString()));
+                        and.printBinary();
+                        break;
+                    case "or":
+                        Instruction or = new Instruction(Instruction.OR_CMD);
+                        or.printBinary();
+                        or.setRD(registers.get(instr.get(1).toString()));
+                        or.setRS(registers.get(instr.get(2).toString()));
+                        or.setRT(registers.get(instr.get(3).toString()));
+                        break;                        
+                    case "add":
+                        Instruction add = new Instruction(Instruction.ADD_CMD);
+                        add.setRD(registers.get(instr.get(1).toString()));
+                        add.setRS(registers.get(instr.get(2).toString()));
+                        add.setRT(registers.get(instr.get(3).toString()));
+                        add.printBinary();
+                        break;
+                    case "addi":
+                        Instruction addi = new Instruction(Instruction.ADDI_CMD);
+                        addi.setRT(registers.get(instr.get(1).toString()));
+                        addi.setRS(registers.get(instr.get(2).toString()));
+                        addi.setImd(Integer.parseInt(instr.get(3).toString()));
+                        addi.printBinary();
+                        break;
+                    case "sll":
+                        Instruction sll = new Instruction(Instruction.SLL_CMD);
+                        sll.setRD(registers.get(instr.get(1).toString()));
+                        sll.setRT(registers.get(instr.get(2).toString()));
+                        sll.setH(Integer.parseInt(instr.get(3).toString()));
+                        sll.printBinary();
+                        break;
+                    case "sub":
+                        Instruction sub = new Instruction(Instruction.SUB_CMD);
+                        sub.setRD(registers.get(instr.get(1).toString()));
+                        sub.setRS(registers.get(instr.get(2).toString()));
+                        sub.setRT(registers.get(instr.get(3).toString()));
+                        sub.printBinary();
+                        break;
+                    case "slt":
+                        Instruction slt = new Instruction(Instruction.SLT_CMD);
+                        slt.setRD(registers.get(instr.get(1).toString()));
+                        slt.setRS(registers.get(instr.get(2).toString()));
+                        slt.setRT(registers.get(instr.get(3).toString()));
+                        slt.printBinary();
+                        break;
+                    case "beq":
+                        Instruction beq = new Instruction(Instruction.BEQ_CMD);
+                        target = instrCount - labels.get(instr.get(3).toString());
+                        beq.setRS(registers.get(instr.get(1).toString()));
+                        beq.setRT(registers.get(instr.get(2).toString()));
+                        beq.setImd(target);
+                        beq.printBinary();
+                        break;                                                                        
+                    case "bne":
+                        Instruction bne = new Instruction(Instruction.BNE_CMD);
+                        target = instrCount - labels.get(instr.get(3).toString());
+                        bne.setRS(registers.get(instr.get(1).toString()));
+                        bne.setRT(registers.get(instr.get(2).toString()));
+                        bne.setImd(target);
+                        bne.printBinary();
+                        break; 
+                    case "lw":
+                        Instruction lw = new Instruction(Instruction.LW_CMD);
+                        lw.setRT(registers.get(instr.get(1).toString()));
+                        lw.setImd(Integer.parseInt(instr.get(2).toString()));
+                        lw.setRS(registers.get(instr.get(3).toString()));
+                        lw.printBinary();
+                        break;                                                                        
+                    case "sw":
+                        Instruction sw = new Instruction(Instruction.SW_CMD);
+                        sw.setRT(registers.get(instr.get(1).toString()));
+                        sw.setImd(Integer.parseInt(instr.get(2).toString()));
+                        sw.setRS(registers.get(instr.get(3).toString()));
+                        sw.printBinary();
+                        break;
+                    case "j":
+                        Instruction j = new Instruction(Instruction.J_CMD);
+                        j.setImd(labels.get(instr.get(1).toString()));
+                        j.printBinary();
+                        break;                                                                        
+                    case "jr":
+                        Instruction jr = new Instruction(Instruction.JR_CMD);
+                        jr.setRS(registers.get(instr.get(1).toString()));
+                        jr.printBinary();
+                        break;                                                     
+                    case "jal":
+                        Instruction jal = new Instruction(Instruction.JAL_CMD);
+                        jal.setImd(labels.get(instr.get(1).toString()));
+                        jal.printBinary();
+                        break;                         
+                    default:
+                }
+            }
+            instrCount++;
+        }
     }
 
     // first pass
@@ -25,16 +137,32 @@ public class asmParser {
 
             // while loop to grab next line in file
             while (scanner.hasNextLine()) {
+                String secondTrim = "";
+                String[] splitString;
                 String ln = scanner.nextLine();
-                // split at : for labels
-                int lst = ln.lastIndexOf(':');
-                // if : detected, add label name + line number to map
-                if (lst != -1) {
-                    String[] result = ln.split(":");
-                    result[0] = result[0].replaceAll("^\\s+",""); //trim leading spaces
-                    labels.put(result[0], lnNum);
+
+                //trim leading and trailing spaces
+                String trimString = ln.trim();
+
+                //split at # for comments
+                splitString = trimString.split("#");
+
+                //Only want to look at everything before the comments
+                if(splitString.length > 0)
+                    secondTrim = splitString[0].trim(); /*remove spaces before # (used to
+                                                          ignore lines that only have comments)*/
+
+                //if the line wasn't empty and wasn't only occupied by comments
+                if(splitString.length > 0 && secondTrim.length() > 0){ 
+                    int lst = splitString[0].lastIndexOf(':');
+                    // if : detected, add label name + line number to map
+                    if (lst != -1) {
+                        String[] result = ln.split(":");
+                        result[0] = result[0].replaceAll("^\\s+",""); //trim leading spaces
+                        labels.put(result[0], lnNum);
+                    }
+                    lnNum += 1;
                 }
-                lnNum += 1;
             }
             scanner.close();
         } catch (FileNotFoundException ex) {
@@ -85,15 +213,18 @@ public class asmParser {
                     }
                 }
             }
-            // printing out list
-            // int c = 1;
-            // for (ArrayList<String> list: instrList) {
-            //     System.out.print("line" + c + ": ");
-            //     for(String s : list) {
-            //        System.out.println(s);
-            //     }
-            //     c++;
-            // }
+
+            //printing out list (Debug)
+            //int c = 1;
+            //System.out.print(instrList);
+            //for (ArrayList<String> list: instrList) {
+                //System.out.print("line" + c + ": ");
+                //System.out.println(list);
+                //for(String s : list) {
+                //   System.out.println(s);
+                //}
+                //c++;
+            //}
 
             scanner.close();
         } catch (FileNotFoundException ex) {
@@ -104,11 +235,11 @@ public class asmParser {
 
     public static Map<String, Integer> getRegisters() {
         // create String array of registers
-        String[] reg = {"zero", "v0", "v1", "a0", "a1", "a2", "a3", "t0", "t1",
+        String[] reg = {"0", "zero", "v0", "v1", "a0", "a1", "a2", "a3", "t0", "t1",
                         "t2", "t3", "t4", "t5", "t6", "t7", "s0", "s1", "s2",
-                        "s3", "s4", "s5", "s6", "s7", "t8", "t9", "ra"};
-        Integer[] regNums = {0, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
-                            16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 31};
+                        "s3", "s4", "s5", "s6", "s7", "t8", "t9", "sp", "ra"};
+        Integer[] regNums = {0, 0, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
+                            16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 29, 31};
 
         // create hashmap and input registers as key and nums as values
         HashMap<String, Integer> registers = new HashMap<>();
