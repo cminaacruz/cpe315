ff8db36567b754b11d26292ff4cd5ca5ab78da5d
Carmina Cruz <cminaacruz@gmail.com>
2019-06-05 15:16:23 -0700

    created functions for printing cache and executing caching (only prints size thus far)

--- lab6/lab6.java
+++ lab6/lab6.java
@@ -11,16 +11,19 @@
                   2KB, 4-way set associative, 4-word blocks
                   4KB, direct mapped, 1-word blocks
 */
+
 import java.util.*;
 import java.io.*;
+import java.lang.Math;
 
 public class lab6 {
     static List<String> addresses = new ArrayList<String>();
     public static void main(String args[]){
+        int KB = 1024;
         /* File parsing that Scans in lines of the file and splits at the tab
            to grab just the addresses and then adds them to a List */
         Scanner readFile;
-        
+
         try {
             readFile = new Scanner(new File(args[0]));
 
@@ -38,7 +41,28 @@ public class lab6 {
         for (int i = 0; i < addresses.size(); i++) {
             System.out.println(addresses.get(i));
         }
+
+        //printCache(1,2,3,4,5,6);
+
+        // 2KB, direct mapped, 1-word blocks
+        cache((2 * KB), 1, 1);
     }
 
+    static void printCache(int cacheNum, int cacheSize, int associativity,
+                            int blockSize, int numHits, int hitRate) {
+        System.out.println("Cache #" + cacheNum);
+        System.out.print("Cache size: " + cacheSize + "B\t");
+        System.out.print("Associativity: " + associativity);
+        System.out.print("\tBlock size: " + blockSize);
+        System.out.print("\nHits: " + numHits);
+        System.out.print("\tHit Rate: " + hitRate + "%");
+        System.out.println("---------------------------");
+    }
+
+    static void cache(int cacheSize, int numWays, int wordBlocks) {
+
+        int cache = (cacheSize/numWays)/(4 * wordBlocks);
+        System.out.println("cache: " + cache);
+    }
 
 }
