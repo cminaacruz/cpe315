13c228f6007b9c07f45ff97598f032e3ae35295a
Carmina Cruz <cminaacruz@gmail.com>
2019-06-09 12:33:04 -0700

    lab6 works and passes test cases

--- lab6/lab6.java
+++ lab6/lab6.java
@@ -48,13 +48,13 @@ public class lab6 {
         directMappedCache(3, addresses, (2 * KB), 4);
 
         // 2KB, 2-way set associative, 1-word blocks
-
+        setAssociativeCache1(4, addresses, (2 * KB), 1, 2);
 
         // 2KB, 4-way set associative, 1-word blocks
-
+        setAssociativeCache2(5, addresses, (2 * KB), 1, 4);
 
         // 2KB, 4-way set associative, 4-word blocks
-
+        setAssociativeCache2(6, addresses, (2 * KB), 4, 4);
 
         // 4KB, direct mapped, 1-word blocks
         directMappedCache(7, addresses, (4 * KB), 1);
@@ -97,9 +97,130 @@ public class lab6 {
             }
             counter++;
         }
-
         double hitRate = ((double)hits/(double)counter) * (double)100;
         printCache(cacheNum, cacheSize, 1, wordBlocks, hits, hitRate);
     }
 
+    static void setAssociativeCache1(int cacheNum, List<String> addressList, int cacheSize, int wordBlocks, int numWays) {
+        int hits = 0;
+        int idxSize = (cacheSize / numWays) / (wordBlocks * 4);
+        int cache[][] = new int[idxSize][numWays];
+        int lru[][] = new int[idxSize][numWays];
+
+        int counter = 0;
+        boolean equalTag = true;
+        while (counter != addressList.size()) {
+            String address = addressList.get(counter);
+            counter++;
+            int intAddress = Integer.parseInt(address, 16);
+            intAddress = intAddress / (wordBlocks * 4);
+            int idx = intAddress % idxSize;
+            int tag = intAddress / idxSize;
+            int ways = 0;
+
+            if (cache[idx][0] == tag) {
+				lru[idx][0] = counter;
+                hits += 1;
+			} else if (cache[idx][1] == tag) {
+				lru[idx][1] = counter;
+                hits += 1;
+			} else {
+                int minIdx = findLRU(numWays, idx, lru);
+				cache[idx][minIdx] = tag;
+				lru[idx][minIdx] = counter;
+			}
+        }
+        double hitRate = ((double)hits/(double)counter) * (double)100;
+        printCache(cacheNum, cacheSize, numWays, wordBlocks, hits, hitRate);
+    }
+
+    static void setAssociativeCache2(int cacheNum, List<String> addressList, int cacheSize, int wordBlocks, int numWays) {
+        int hits = 0;
+        int idxSize = (cacheSize / numWays) / (wordBlocks * 4);
+        int cache[][] = new int[idxSize][numWays];
+        int lru[][] = new int[idxSize][numWays];
+
+        int counter = 0;
+        boolean equalTag = true;
+        while (counter != addressList.size()) {
+            String address = addressList.get(counter);
+            counter++;
+            int intAddress = Integer.parseInt(address, 16);
+            intAddress = intAddress / (wordBlocks * 4);
+            int idx = intAddress % idxSize;
+            int tag = intAddress / idxSize;
+            int ways = 0;
+
+            if (cache[idx][0] == tag) {
+				lru[idx][0] = counter;
+                hits += 1;
+			} else if (cache[idx][1] == tag) {
+				lru[idx][1] = counter;
+                hits += 1;
+			} else if (cache[idx][2] == tag) {
+				lru[idx][2] = counter;
+                hits += 1;
+			} else if (cache[idx][3] == tag) {
+				lru[idx][3] = counter;
+                hits += 1;
+			} else {
+                int minIdx = findLRU(numWays, idx, lru);
+				cache[idx][minIdx] = tag;
+				lru[idx][minIdx] = counter;
+			}
+        }
+        double hitRate = ((double)hits/(double)counter) * (double)100;
+        printCache(cacheNum, cacheSize, numWays, wordBlocks, hits, hitRate);
+    }
+
+    static void setAssociativeCache3(int cacheNum, List<String> addressList, int cacheSize, int wordBlocks, int numWays) {
+        int hits = 0;
+        int idxSize = (cacheSize / numWays) / (wordBlocks * 4);
+        int cache[][] = new int[idxSize][numWays];
+        int lru[][] = new int[idxSize][numWays];
+
+        int counter = 0;
+        boolean equalTag = true;
+        while (counter != addressList.size()) {
+            String address = addressList.get(counter);
+            counter++;
+            int intAddress = Integer.parseInt(address, 16);
+            intAddress = intAddress / (wordBlocks * 4);
+            int idx = intAddress % idxSize;
+            int tag = intAddress / idxSize;
+            int ways = 0;
+
+            if (cache[idx][0] == tag) {
+				lru[idx][0] = counter;
+                hits += 1;
+			} else if (cache[idx][1] == tag) {
+				lru[idx][1] = counter;
+                hits += 1;
+			} else if (cache[idx][2] == tag) {
+				lru[idx][2] = counter;
+                hits += 1;
+			} else if (cache[idx][3] == tag) {
+				lru[idx][3] = counter;
+                hits += 1;
+			} else {
+                int minIdx = findLRU(numWays, idx, lru);
+				cache[idx][minIdx] = tag;
+				lru[idx][minIdx] = counter;
+			}
+        }
+        double hitRate = ((double)hits/(double)counter) * (double)100;
+        printCache(cacheNum, cacheSize, numWays, wordBlocks, hits, hitRate);
+    }
+
+    static int findLRU(int numWays, int idx, int[][] lru) {
+        ArrayList<Integer> lineNums = new ArrayList<Integer>();
+        int minIndex = 0;
+
+        for(int i = 0; i < numWays; i++)
+        {
+            lineNums.add(lru[idx][i]);
+        }
+        minIndex = lineNums.indexOf(Collections.min(lineNums));
+        return minIndex;
+    }
 }
